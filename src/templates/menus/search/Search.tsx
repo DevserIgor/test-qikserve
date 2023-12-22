3914721e8f5f0e58c56e179ec81d124aa5c0b34e
Igor Silva <devserigor@gmail.com>
2023-12-22 13:02:43 -0300

    feature: implemented search component

--- src/templates/menus/search/Search.tsx
+++ src/templates/menus/search/Search.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
-interface Props {
-  // Add your component props here
-}
-
-const Search: React.FC<Props> = () => {
-  // Add your component logic here
-
+const Search: React.FC = () => {
   return (
-    <div className="p-4">
+    <div className="relative p-4">
+      <MdSearch className="absolute left-[30px] top-[38px] transform -translate-y-1/2 text-[#8A94A4] z-10 font-light text-[20px]" />
       <input
-        className="w-full p-3 border rounded"
+        className="w-full pl-10 h-[40px] rounded-[7.29px] border-[0.91px] border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none text-gray-700 font-roboto text-base font-normal leading-[18.24px] tracking-normal text-left"
         type="search"
         placeholder="Search menu items"
       />
