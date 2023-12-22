384f5d8f598b5804b49714e1ab2be1eca40afb33
Igor Silva <devserigor@gmail.com>
2023-12-22 02:46:25 -0300

    build: new configs and library to styles in js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,11 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'hero-banner': 'var(--banner-image)'
+      },
+      colors: {
+        'my-color-red': '#FF0000'
       }
     }
   },
